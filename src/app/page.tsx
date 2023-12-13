7b97a5486f5d2a5aab5df52e839c6f09efbfe67b
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2023-12-13 19:27:55 +0000

    feat: add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
-import Image from "next/image";
+import Form from "./components/form/Form";
+import Label from "./components/label/Label";
+import Input from "./components/input/Input";
+import Button from "./components/button/Button";
+import Container from "./components/container/Container";
+import FormWrapper from "./components/wrapper/FormWrapper";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="min-h-screen">
+      <Container>
+        <FormWrapper>
+          <h1 className="text-xl font-bold">Sign in</h1>
+          <Form>
+            <Label inputId={"email"}>
+              Email
+              <Input
+                type={"email"}
+                name={"email"}
+                id={"email"}
+                placeholder={"Email"}
+              />
+            </Label>
+            <Label inputId={"password"}>
+              Password
+              <Input
+                type={"password"}
+                name={"password"}
+                id={"password"}
+                placeholder={"Password"}
+              />
+            </Label>
+            <Button typeButton={"submit"} text={"Sign in"} />
+          </Form>
+        </FormWrapper>
+      </Container>
+    </main>
   );
 }
