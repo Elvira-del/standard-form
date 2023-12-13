1967cf94309cb009652eca682e70e9273cd3f252
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2023-12-13 19:25:25 +0000

    refactor: change styles

--- src/app/components/container/Container.tsx
+++ src/app/components/container/Container.tsx
@@ -6,7 +6,7 @@ type ContainerType = {
 
 export default function Container({ children }: ContainerType) {
   return (
-    <div className="max-w-sm w-full mx-auto px-4 sm:max-w-md sm:px-7 md:max-w-lg md:px-12 lg:max-w-3xl xl:max-w-5xl xl:px-36 2xl:max-w-7xl">
+    <div className="max-w-sm w-full min-h-screen mx-auto px-4 sm:max-w-md sm:px-7 md:max-w-lg md:px-12 lg:max-w-3xl xl:max-w-5xl xl:px-36 2xl:max-w-7xl">
       {children}
     </div>
   );
--- src/app/components/wrapper/FormWrapper.tsx
+++ src/app/components/wrapper/FormWrapper.tsx
@@ -6,7 +6,7 @@ type FormWrapperType = {
 
 export default function FormWrapper({ children }: FormWrapperType) {
   return (
-    <div className="flex flex-col items-center justify-between gap-y-8 mt-20 p-6 border rounded-md sm:p-12 md:p-20">
+    <div className="absolute top-1/4 left-1/3 flex flex-col items-center justify-between gap-y-8 p-6 border rounded-md sm:p-12 md:p-20">
       {children}
     </div>
   );
